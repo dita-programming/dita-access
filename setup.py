f34e9cff55b1963019485b460b69f750745d7f83
michael <michaelbukachi@gmail.com>
2015-03-31 14:52:43 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Dita Access',
+    version='1.3',
+    packages=['view', 'model', 'controller'],
+    url='',
+    license='GPL v3',
+    author='michael',
+    author_email='michaelbukachi@gmail.com',
+    description='Access system for entry into DITA resource lab'
+)
